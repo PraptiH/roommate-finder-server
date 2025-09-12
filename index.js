65e5777ba323b2e93f07ea60cfafb79cb0509843
PraptiH <praptiantara1892@gmail.com>
2025-09-12 14:42:27 +0600

    create server for roommate finder website

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const cors = require('cors');
+const port = process.env.PORT || 3000
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('RoommateFinder Server is Running!!!!')
+ })
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
