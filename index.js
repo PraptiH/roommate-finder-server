57d4e60ea985b4d20273e7ece88b4913029547e4
PraptiH <praptiantara1892@gmail.com>
2025-09-27 19:52:42 +0600

    Implement delete functionality on the server

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/posts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await postsCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // users API
 
     const usersCollection = client.db('roommatefinder').collection("users")
